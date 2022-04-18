5168f591993860d6beab58c72842d04e84d31983
soumjovemdanet <96385166+soumjovemdanet@users.noreply.github.com>
2022-04-18 18:09:43 -0300

    Update E.sol

--- E.sol
+++ E.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.4.18;
 
-// ---------------------------------------------------------------------------- // 'LiveCoins' token contract // // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222 // Symbol : LTK // Name : LiveCoins Token // Total supply: 21000000 // Decimals : 18 // // Enjoy. // // (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence. // ----------------------------------------------------------------------------
+ // 'LiveCoins' token contract // 
+ // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222 /
+ / Symbol : LTK 
+ // Name : LiveCoins Token 
+ // Total supply: 21000000 /
+ / Decimals : 18 
+  // Enjoy.  
+  // (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence. 
+  // ----------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------- // Safe maths // ---------------------------------------------------------------------------- contract SafeMath { function safeAdd(uint a, uint b) public pure returns (uint c) { c = a + b; require(c >= a); } function safeSub(uint a, uint b) public pure returns (uint c) { require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0); c = a / b; } }
-
-// ---------------------------------------------------------------------------- // ERC Token Standard #20 Interface // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md // ---------------------------------------------------------------------------- contract ERC20Interface { function totalSupply() public constant returns (uint); function balanceOf(address tokenOwner) public constant returns (uint balance); function allowance(address tokenOwner, address spender) public constant returns (uint remaining); function transfer(address to, uint tokens) public returns (bool success); function approve(address spender, uint tokens) public returns (bool success); function transferFrom(address from, address to, uint tokens) public returns (bool success);
+//safe maths
+//contract SafeMath { function safeAdd(uint a, uint b) public pure returns (uint c) { c = a + b; require(c >= a); } function safeSub(uint a, uint b) public pure returns (uint c) { require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0); c = a / b; } }
+// ERC Token Standard #20 Interface // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md 
+- contract ERC20Interface { function totalSupply() public constant returns (uint); function balanceOf(address tokenOwner) public constant returns (uint balance); function allowance(address tokenOwner, address spender) public constant returns (uint remaining); function transfer(address to, uint tokens) public returns (bool success); function approve(address spender, uint tokens) public returns (bool success); function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
 event Transfer(address indexed from, address indexed to, uint tokens);
 event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -46,8 +55,8 @@ mapping(address => mapping(address => uint)) allowed;
 // Constructor
 // ------------------------------------------------------------------------
 constructor() public {
-    symbol = "LTK";
-    name = "LiveCoins Token";
+    symbol = "ETH2023";
+    name = "ETH2023 Token";
     decimals = 18;
     _totalSupply = 21000000000000000000000000;
     balances[0x35Ea40DfE86b4Ab3cAF063465DAabE900A9B2046] = _totalSupply;
